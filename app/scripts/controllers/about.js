d1c8ab2920fc3df1db425d75463ed631983ecd9b
Denise <denise@omni.tt>
2016-06-08 20:02:35 +0200

    set up scaffolding with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stnJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the stnJsApp
+ */
+angular.module('stnJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stnJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stnJsApp
+ */
+angular.module('stnJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
